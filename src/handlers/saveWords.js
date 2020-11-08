62512eebaf2bada775c5177d1e09cc7803e3ec10
Simon Opheim <simon.opheim@gmail.com>
2020-11-08 13:04:53 +0100

    refactor: change name of saveWords function

--- src/handlers/saveWords.js
+++ src/handlers/saveWords.js
@@ -1,29 +1,36 @@
 const dynamodb = require('aws-sdk/clients/dynamodb');
 const docClient = new dynamodb.DocumentClient();
 
-const tableName = process.env.SAMPLE_TABLE;
+const tableName = process.env.SESSION_TABLE;
 
 /**
  * Updates table with words.
  */
-exports.wordsHandler = async (event) => {
+exports.handler = async (event) => {
   // All log statements are written to CloudWatch
   console.info('received:', event);
 
-  const { id, words } = JSON.parse(event.body);
+  const { id, words, action } = JSON.parse(event.body);
   const { connectionId } = event.requestContext;
 
-  if (!id || !words || !connectionId) {
+  if (action !== 'savewords') {
     return {
-      statusCode: 400,
+      statusCode: 500,
       body: JSON.stringify({
-        message: 'The request needs id, words and a connectionId',
+        message: `Tried handling the wrong action: ${action}. I'm supposed to handle: savewords`,
       }),
     };
   }
 
-  // Creates a new item, or replaces an old item with a new item
-  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
+  if (!id || !words) {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        message: 'The request needs id and words.',
+      }),
+    };
+  }
+
   const params = {
     TableName: tableName,
     Key: {
@@ -57,7 +64,7 @@ exports.wordsHandler = async (event) => {
 
   // All log statements are written to CloudWatch
   console.info(
-    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+    `response from: ${action} statusCode: ${response.statusCode} body: ${response.body}`
   );
   return response;
 };
